fe36b42d0e00904a4adf075bafa307087f88044c
Kamil Balda <baldakamil@gmail.com>
2020-03-30 11:09:53 +0200

    printing function added

--- src/Components/Dashboard/DietPlan/DayColumnFree/DietPlanColumnPrint.js
+++ src/Components/Dashboard/DietPlan/DayColumnFree/DietPlanColumnPrint.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-function DietPlanColumnPrint(props){
+function DietPlanColumnPrint(){
+
+  const printPage = () => {
+    window.print()
+  }
+
   let backgroundStyle = { 
     backgroundImage: `url(${process.env.PUBLIC_URL}/img/plan_background_bottom_corner.png)`
   }
     return(
-    <div style={backgroundStyle} className="plan__item-print">
+    <div style={backgroundStyle} className="plan__item-print" onClick={ printPage }>
       <img src={`${process.env.PUBLIC_URL}/img/printer_ico.svg`} alt="printer" className="p-13"/>
       <p className="color-a2a2a2">Print</p>
     </div>
