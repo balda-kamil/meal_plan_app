aa64fa7e2532b3c1bfb7172613677ebf37c1ddd8
Kamil Balda <baldakamil@gmail.com>
2020-03-30 09:19:08 +0200

    hamburger menu added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,16 +2,23 @@ import React from 'react'
 import './Navbar.css'
 import NavbarSingleLink from './NavbarSingleLink/NavbarSingleLink'
 import NavbarUserRightContainer from './NavbarUserRightContainer/NavbarUserRightContainer'
+import { slide as HamburgerMenu } from 'react-burger-menu'
 
-function Navbar(){
+function Navbar(props){
+  console.log(props)
   return(
     <div className="nav__container">
       <nav>
-        <ul className="nav__links">
-          <NavbarSingleLink linkTo="/" text="Dashboard"/>
-          <NavbarSingleLink linkTo="/recipies" text="Recipies"/>
-          <NavbarSingleLink linkTo="/challenge" text="Challenge"/>
-        </ul>
+        <HamburgerMenu>
+          <NavbarSingleLink linkTo="/" text="Dashboard" className="menu-item"/>
+          <NavbarSingleLink linkTo="/recipies" text="Recipies" className="menu-item"/>
+          <NavbarSingleLink linkTo="/challenge" text="Challenge" className="menu-item"/>
+        </HamburgerMenu>
+      <ul className="nav__links">
+        <NavbarSingleLink linkTo="/" text="Dashboard"/>
+        <NavbarSingleLink linkTo="/recipies" text="Recipies"/>
+        <NavbarSingleLink linkTo="/challenge" text="Challenge"/>
+      </ul>
         <NavbarUserRightContainer/>
       </nav>
     </div>
